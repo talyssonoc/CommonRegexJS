c12ff17e97eaaa12bfadfce159b43672340b3233
talyssonoc <a11023@bcc.unifal-mg.edu.br>
2013-12-26 16:15:40 -0200

    Put regexes in CommonRegex scope.

--- commonregex.js
+++ commonregex.js
@@ -21,65 +21,70 @@
       return regexes.join('|');
     };
 
-    this.getDates = function(_text) {
-      _text = _text || this.text;
+    var dateRegex;
+
+    (function() {
       var monthRegex = '(?:jan\\.?|january|feb\\.?|february|mar\\.?|march|apr\\.?|april|may|jun\\.?|june|jul\\.?|july|aug\\.?|august|sep\\.?|september|oct\\.?|october|nov\\.?|november|dec\\.?|december)';
       // var dayRegex = '(?<!\\:)(?<!\\:\\d)[0-3]?\\d(?:st|nd|rd|th)?'; Any alternative for lookbehind in JavaScript ?
       var dayRegex = '[0-3]?\\d(?:st|nd|rd|th)?';
       var yearRegex = '\\d{4}';
 
-      var dateRegex = group(any([dayRegex + '\\s+(?:of\\s+)?' + monthRegex, monthRegex + '\\s+' + dayRegex])) + '(?:\\,)?\\s*' + opt(yearRegex) + '|[0-3]?\\d[-/][0-3]?\\d[-/]\\d{2,4}';
+      dateRegex = group(any([dayRegex + '\\s+(?:of\\s+)?' + monthRegex, monthRegex + '\\s+' + dayRegex])) + '(?:\\,)?\\s*' + opt(yearRegex) + '|[0-3]?\\d[-/][0-3]?\\d[-/]\\d{2,4}';
 
       dateRegex = new RegExp(dateRegex, 'gim');
+    })();
 
+    var timeRegex = /((0?[0-9]|1[0-2]):[0-5][0-9](am|pm)|([01]?[0-9]|2[0-3]):[0-5][0-9])/gim;
+    var phoneRegex = /(\d?[^\s\w]*(?:\(?\d{3}\)?\W*)?\d{3}\W*\d{4})/gim;
+    var linksRegex = /((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))/gim;
+    var emailsRegex = /([a-z0-9!#$%&'*+\/=?^_`{|}~-]+@([a-z0-9]+\.)+([a-z0-9]+))/gim;
+    var IPv4Regex = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/gm;
+    var hexValuesRegex = /#(?:[0-9a-fA-F]{3}){1,2}\b/gim;
+    var acronymsRegex = /\b(([A-Z]\.)+|([A-Z]){2,})/gm;
+    var moneyRegex = /((^|\b)US?)?\$\s?[0-9]{1,3}((,[0-9]{3})+|([0-9]{3})+)?(\.[0-9]{1,2})?\b/gm;
+
+    this.getDates = function(_text) {
+      _text = _text || this.text;
       return _text.match(dateRegex);
     };
 
     this.getTimes = function(_text) {
       _text = _text || this.text;
-      var timeRegex = /((0?[0-9]|1[0-2]):[0-5][0-9](am|pm)|([01]?[0-9]|2[0-3]):[0-5][0-9])/gim;
       return _text.match(timeRegex);
     };
 
     this.getPhones = function(_text) {
       _text = _text || this.text;
-      var phoneRegex = /(\d?[^\s\w]*(?:\(?\d{3}\)?\W*)?\d{3}\W*\d{4})/gim;
       return _text.match(phoneRegex);
     };
 
     this.getLinks = function(_text) {
       _text = _text || this.text;
-      var linksRegex = /((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\))+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))/gim;
       return _text.match(linksRegex);
     };
 
     this.getEmails = function(_text) {
       _text = _text || this.text;
-      var emailsRegex = /([a-z0-9!#$%&'*+\/=?^_`{|}~-]+@([a-z0-9]+\.)+([a-z0-9]+))/gim;
       return _text.match(emailsRegex);
     };
 
     this.getIPv4 = function(_text) {
       _text = _text || this.text;
-      var IPv4Regex = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/gm;
       return _text.match(IPv4Regex);
     };
 
     this.getHexColors = function(_text) {
       _text = _text || this.text;
-      var hexValuesRegex = /#(?:[0-9a-fA-F]{3}){1,2}\b/gim;
       return _text.match(hexValuesRegex)
     };
 
     this.getAcronyms = function(_text) {
       _text = _text || this.text;
-      var acronymsRegex = /\b(([A-Z]\.)+|([A-Z]){2,})/gm;
       return _text.match(acronymsRegex);
     };
 
     this.getMoney = function(_text) {
       _text = _text || this.text;
-      var moneyRegex = /((^|\b)US?)?\$\s?[0-9]{1,3}((,[0-9]{3})+|([0-9]{3})+)?(\.[0-9]{1,2})?\b/gm;
       return _text.match(moneyRegex);
     };
 
